59d74fbe2f6444520218ee3e232ebafa7c03e043
NicolasWltr <nicolas-walter1@gmx.de>
2025-05-05 02:23:37 +0200

    Added animation to qualification + installed BreakpointObserver to check current breakpoint for correct animation

--- src/app/components/qualification/qualification.component.ts
+++ src/app/components/qualification/qualification.component.ts
@@ -1,14 +1,33 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AnimateOnEnterComponent } from "../animate-on-enter/animate-on-enter.component";
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-qualification',
   imports: [
-    CommonModule
-  ],
+    CommonModule,
+    AnimateOnEnterComponent
+],
   templateUrl: './qualification.component.html',
   styleUrl: './qualification.component.scss'
 })
-export class QualificationComponent {
+export class QualificationComponent implements OnInit {
   @Input() work: boolean = false;
+  public split: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    
+  }
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe('(min-width: 768px)').subscribe((state: BreakpointState) => {
+      console.log(state);
+      if (state.matches) {
+        this.split = false;
+      } else {
+        this.split = true;
+      }
+    });
+  }
 }
